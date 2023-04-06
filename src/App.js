a5f98f7edfe427d2ef8c2746f6fe1642f6326b65
mdkaifp1 <95919428+mdkaifp1@users.noreply.github.com>
2023-04-06 17:57:37 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./styles.css";
+import "./index.css";
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Feature,
+  Header,
+  WhatGPT3
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Feature />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Article } from "./articles/article";
+export { default as Brand } from "./brand/brand";
+export { default as CTA } from "./cta/cta";
+export { default as Feature } from "./feature/features";
+export { default as Navbar } from "./navbar/Navbar";
